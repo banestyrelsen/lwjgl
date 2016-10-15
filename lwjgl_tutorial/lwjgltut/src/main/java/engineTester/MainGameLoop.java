ab031ff062971c47ae221128b365ddad0bd09f81
banestyrelsen <valdemar.johansen@sigma.se>
2016-10-15 20:27:03 +0200

    6: Textures

--- lwjgl_tutorial/lwjgltut/src/main/java/engineTester/MainGameLoop.java
+++ lwjgl_tutorial/lwjgltut/src/main/java/engineTester/MainGameLoop.java
@@ -2,11 +2,13 @@ package engineTester;
 
 import org.lwjgl.opengl.Display;
 
+import models.RawModel;
+import models.TexturedModel;
 import renderEngine.DisplayManager;
 import renderEngine.Loader;
-import renderEngine.RawModel;
 import renderEngine.Renderer;
 import shaders.StaticShader;
+import textures.ModelTexture;
 
 public class MainGameLoop {
 
@@ -16,33 +18,42 @@ public class MainGameLoop {
 		Loader loader = new Loader();
 		Renderer renderer = new Renderer();
 		StaticShader shader = new StaticShader();
-		
+
+		// Storing position VBO in VAO attribute 0
 		// OpenGL expects vertices to be defined counter clockwise by default
-		float[] vertices = {
-			-0.5f, 0.5f, 0f,	// V1
-			-0.5f, -0.5f, 0f,	// V2
-			0.5f, -0.5f, 0f,	// V3
-			0.5f, 0.5f, 0f		// V4
+		float[] vertices = { -0.5f, 0.5f, 0f, // V1
+				-0.5f, -0.5f, 0f, // V2
+				0.5f, -0.5f, 0f, // V3
+				0.5f, 0.5f, 0f // V4
 		};
-		
-		int[] indices = {
-				0,1,3,	// Top left triangle (V0,V1,V3)
-				3,1,2	// Bottom right triangle (V3,V1,V2)
+
+		int[] indices = { 0, 1, 3, // Top left triangle (V0,V1,V3)
+				3, 1, 2 // Bottom right triangle (V3,V1,V2)
 		};
-		
-		RawModel model = loader.loadToVAO(vertices, indices);
-		
+
+		// Storing texture coordinates VBO in VAO attribute 1
+		float[] textureCoords = {
+				0, 0, // V0
+				0, 1, // V1
+				1, 1, // V2
+				1, 0  // V3
+		};
+
+		RawModel model = loader.loadToVAO(vertices, textureCoords, indices);
+		ModelTexture texture = new ModelTexture(loader.loadTexture("spots"));
+		TexturedModel texturedModel = new TexturedModel(model, texture);
+
 		while (!Display.isCloseRequested()) {
 			renderer.prepare();
 			shader.start();
-			renderer.render(model);
+			renderer.render(texturedModel);
 			shader.stop();
 			DisplayManager.updateDisplay();
 		}
-		
+
 		shader.cleanUp();
 		loader.cleanUp();
-		
+
 		DisplayManager.closeDisplay();
 	}
 
---
+++ lwjgl_tutorial/lwjgltut/src/main/java/textures/ModelTexture.java
@@ -0,0 +1,15 @@
+package textures;
+
+public class ModelTexture {
+
+	private int textureId;
+	
+	public ModelTexture(int id){
+		this.textureId = id;
+	}
+
+	public int getTextureId() {
+		return textureId;
+	}
+	
+}
